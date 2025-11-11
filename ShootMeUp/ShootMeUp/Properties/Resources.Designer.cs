2e21a5fd9bfae7effade3f082f3823d45b90c568
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2025-11-11 11:04:21 +0100

    chore(Bush): Added a bush texture

--- ShootMeUp/ShootMeUp/Properties/Resources.Designer.cs
+++ ShootMeUp/ShootMeUp/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace ShootMeUp.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ObstacleBush {
+            get {
+                object obj = ResourceManager.GetObject("ObstacleBush", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ObstacleNormal {
             get {
                 object obj = ResourceManager.GetObject("ObstacleNormal", resourceCulture);
